0792bc95b70b5d801261f77bc5db2978274548ad
Ruutra69 <95411160+Ruutra69@users.noreply.github.com>
2022-01-27 12:56:25 +0100

    Add files via upload

---
+++ MinecraftServerScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install default-jre
+wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar -O minecraft_server.1.18.1.jar
+chmod +x /home/minecraft/run.sh
